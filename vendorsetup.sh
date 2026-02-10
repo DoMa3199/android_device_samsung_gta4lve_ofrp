9817569e1a8fa69a0953e2d38149fe48ab798f2a
DevCat3 <omarelbakery2008@gmail.com>
2026-02-10 20:41:02 +0200

    added lzma

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -36,3 +36,4 @@ export FOX_RECOVERY_VENDOR_PARTITION="/dev/block/mapper/by-name/vendor"
 export FOX_RECOVERY_SYSTEM_EXT_PARTITION="/dev/block/mapper/by-name/system_ext"
 export FOX_RECOVERY_PRODUCT_PARTITION="/dev/block/mapper/by-name/product"
 export FOX_RECOVERY_BOOT_PARTITION="/dev/block/by-name/boot"
+export OF_USE_LZMA_COMPRESSION=1
